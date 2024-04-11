f9a6397a37b29280bca4c786cf0c062c4251eaf3
MichalEder <edermichal.eder@gmail.com>
2024-04-11 11:39:21 +0200

    added api for station and station annual

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import numpy as np
 
 app = Flask(__name__)
 
+stations = pd.read_csv('data/stations.txt', skiprows=17)
+stations = stations[['STAID', 'STANAME                                 ', 'CN']]
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', data=stations.to_html())
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
@@ -30,7 +32,25 @@ def about(station, date):
         'temperature_max': temperature_max,
         'temperature_min': temperature_min,
     }
-    return response #render_template('api')
+    return response
+
+@app.route('/api/v1/<station>')
+def all_data(station):
+    filename = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
+    result = df.to_dict(orient='records')
+    return result
+
+@app.route('/api/v1/annual/<station>/<year>')
+def annual_data(station, year):
+    filename = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
+    df = pd.read_csv(filename, skiprows=20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient='records')
+    return result
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
