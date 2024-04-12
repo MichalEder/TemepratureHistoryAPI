2187bcd1f99a0eac10890911ed8dc0f6996a72dc
MichalEder <edermichal.eder@gmail.com>
2024-04-12 16:34:40 +0200

    try with sql

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 import pandas as pd
 import numpy as np
+import sqlite3
 
 app = Flask(__name__)
 
-stations = pd.read_csv('data/stations.txt', skiprows=17)
+stations = pd.read_csv('data/data_misc/stations.txt', skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ', 'CN']]
 
 @app.route('/')
@@ -13,15 +14,22 @@ def home():
 
 @app.route('/api/v1/<station>/<date>')
 def about(station, date):
-    filename = 'data/TG_STAID' + str(station).zfill(6) + '.txt'
-    df = pd.read_csv(filename, skiprows=20, parse_dates=['    DATE'])
-    df['TG0'] = df['   TG'].mask(df['   TG'] == -9999, np.nan)
-    df['TG'] = df['TG0'] / 10
+    conn = sqlite3.connect('data/ecad.db')
+    table_name = 'TG_STAID' + str(station).zfill(6)
+    # # query = f'''
+    # # SELECT TG FROM {table_name}
+    # # WHERE date = ?
+    # # '''
+    # df = pd.read_sql(query, conn, params=[date])
+    df = pd.read_sql_table(table_name, conn)
+
+    df['tg0'] = df['tg'].mask(df['tg'] == -9999, np.nan)
+    df['tg'] = df['tg0'] / 10
     modified_date = date[0:4] + '-' + date[4:6] + '-' + date[6:9]
-    temperature = df.loc[df['    DATE'] == modified_date]['TG'].squeeze()
-    temperature_mean = df['TG'].mean()
-    temperature_min = df['TG'].min()
-    temperature_max = df['TG'].max()
+    temperature = df.loc[df['date'] == modified_date]['tg'].squeeze()
+    temperature_mean = df['tg'].mean()
+    temperature_min = df['tg'].min()
+    temperature_max = df['tg'].max()
 
 
     response = {
